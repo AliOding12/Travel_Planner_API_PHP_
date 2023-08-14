bc05582f288ed5e097c4127fa8feb2bb4929f26f
AliOding12 <aliz17043@gmail.com>
2023-08-14 17:00:00 +0500

    Add InvalidRequestException for handling invalid API requests

--- app/Exceptions/InvalidRequestException.php
+++ app/Exceptions/InvalidRequestException.php
@@ -3,7 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
-
+//right now it is the only requirement 
 class InvalidRequestException extends Exception
 {
     public function __construct(string $message, int $code = 400)
