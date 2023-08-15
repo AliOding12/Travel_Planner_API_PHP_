4cbaa7fcf7ed4e697bdff13191d2338f0fa5def9
AliOding12 <aliz17043@gmail.com>
2023-08-15 15:30:00 +0500

    Add ThirdPartyApiException for handling external API errors

--- app/Exceptions/ThirdPartyApiException.php
+++ app/Exceptions/ThirdPartyApiException.php
@@ -3,7 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
-
+//for further exceptions like key mismatch create a exception of that here 
 class ThirdPartyApiException extends Exception
 {
     public function __construct(string $message, int $code = 502)
