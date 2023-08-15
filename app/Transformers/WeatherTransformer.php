fbd89db1fe908e56cfed05eb4759555e3db9bcd3
AliOding12 <aliz17043@gmail.com>
2023-08-15 12:30:00 +0500

    Implement WeatherTransformer for normalizing weather API responses

--- app/Transformers/WeatherTransformer.php
+++ app/Transformers/WeatherTransformer.php
@@ -4,6 +4,7 @@ namespace App\Transformers;
 
 class WeatherTransformer
 {
+// for future and a bigger picture of such api server i tend to create a utils file and store all these transformers there if you can please contribute
 
     public function transform(array $data): array
     {
